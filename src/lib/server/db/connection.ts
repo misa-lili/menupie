9955233bc97730d44e4e337a88eaed18b766a11d
kyom <hakgeumkim@gmail.com>
2024-03-22 13:31:46 +0900

    feat: docker

--- src/lib/server/db/connection.ts
+++ src/lib/server/db/connection.ts
@@ -1,13 +1,13 @@
-import { env } from "$env/dynamic/private"
+import { PGUSER, PGHOST, PGDB, PGPW, PGPORT } from "$env/static/private"
 const pkg = await import("pg")
 const { Pool } = pkg.default
 
 const config = {
-  user: env.PGUSER,
-  host: env.PGHOST,
-  database: env.PGDB,
-  password: env.PGPW,
-  port: Number(env.PGPORT),
+  user: PGUSER,
+  host: PGHOST,
+  database: PGDB,
+  password: PGPW,
+  port: Number(PGPORT),
 }
 
 export const pool = new Pool(config)
