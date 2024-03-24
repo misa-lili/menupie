f1c033e8d6438443d630597c42f399b2a03444e8
kyom <hakgeumkim@gmail.com>
2024-03-25 02:04:59 +0900

    feat: 기존 컨테이너 중지

--- run-docker.sh
+++ run-docker.sh
@@ -4,7 +4,15 @@
 current_dir=$(basename "$PWD")
 
 # 도커 이미지 빌드
+echo "Building $current_dir image..."
 docker build -t "$current_dir" .
 
+# 기존에 실행 중인 컨테이너 중지
+echo "Stopping and removing existing $current_dir container..."
+docker stop "$current_dir" >/dev/null 2>&1
+
 # 도커 컨테이너 실행
+echo "Running $current_dir container..."
 docker run -d --network host --name "$current_dir" --restart=always "$current_dir"
+
+echo "Done."
